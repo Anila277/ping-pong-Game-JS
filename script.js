290677be6135b6d4f7e808d592becbd644d86cd7
Anila Nisar <raniyasr@gmail.com>
2023-01-07 22:01:53 -0500

    wip: adds mousemove

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 import Ball from "./ball.js"
+import Paddle from "./paddle.js"
 
 const ball = new Ball(document.getElementById('ball'))
+const playerPaddle = new Paddle(document.getElementById('player-paddle'))
+const computerPaddle = new Paddle(document.getElementById('computer-paddle'))
 
 let lastTime
 function update(time) {
@@ -12,5 +15,9 @@ function update(time) {
     lastTime = time
     window.requestAnimationFrame(update)
 }
+
+document.addEventListener('mousemove', e => {
+    playerPaddle.position = (e.y / window.innerHeight) * 100
+})
 
 window.requestAnimationFrame(update)
