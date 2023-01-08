fd71f30483c06be64b3a913a4ca71db1e0dd25ad
Anila Nisar <raniyasr@gmail.com>
2023-01-07 22:35:43 -0500

    wip: fixed a bug

--- ball.js
+++ ball.js
@@ -50,7 +50,7 @@ export default class Ball {
         if (rect.bottom >= window.innerHeight || rect.top <= 0) {
             this.direction.y *= -1
         }
-        if (rect.right >= window.innerHeight || rect.left <= 0) {
+        if (rect.right >= window.innerWidth || rect.left <= 0) {
             this.direction.x *= -1
         }
     }
